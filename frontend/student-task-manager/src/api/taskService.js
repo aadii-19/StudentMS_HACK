b7da9d948fe0db30545cbfd297e2aecd44d526c4
Navaneetha Krishna <krishnaofficial2026@gmail.com>
2026-02-09 19:57:21 +0530

    last base url modification

--- frontend/student-task-manager/src/api/taskService.js
+++ frontend/student-task-manager/src/api/taskService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api/tasks';
+const API_BASE_URL = 'http://localhost:9090/api/tasks';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
